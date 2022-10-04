a1939eefbf5ed97eccd85fc7c86197b4d5a12f17
Diego Collado <108297115+DColladoRK@users.noreply.github.com>
2022-10-03 21:51:55 -0500

    Style loaders main configuration

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import "./styles/index.css";
+import "./styles/index.scss";
 const randomAux = {
     aux1 : 1,
     aux2 : 2,
--- webpack.config.js
+++ webpack.config.js
@@ -1,15 +1,18 @@
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 let mode = "development";
+let target = "web";
 if (process.env.NODE_ENV === "production") {
     mode = "production";
+    target = "browserslist";
 }
 module.exports = {
     mode: mode,
+    target: target,
     module: {
         rules: [
             {
-                test: /\.css$/i,
-                use: [MiniCssExtractPlugin.loader, "css-loader"],
+                test: /\.s?css$/i,
+                use: [MiniCssExtractPlugin.loader, "css-loader", "postcss-loader", "sass-loader"],
             },
             {
                 test: /\.js$/,
@@ -24,5 +27,6 @@ module.exports = {
     devtool: "source-map",
     devServer: {
         static: "./dist",
+        hot: true,
     }
 }
