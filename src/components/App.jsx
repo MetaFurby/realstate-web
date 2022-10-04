99ac745809b96ff6715921f9c641b1a5a002e7af
Diego Collado <108297115+DColladoRK@users.noreply.github.com>
2022-10-04 10:15:14 -0500

    Added resources config

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,15 @@
 import "../styles/index.scss";
 import Recipes from "./Recipes";
-
+import houseImg from "../assets/house-1.png"
 const App = () => {
     return(
         <>
-            <section className="hero"></section>
+            <section className="house"></section>
             <main>
                 <section>
                     <h1>hi react</h1>
                 </section>
+                <img src={houseImg} alt="houseImg" width="250"></img>
             </main>
             <Recipes></Recipes>
         </>
