df5b9ca7da3fbe84a7baaf7516589c6033e04b1a
Diego Collado <95520927+MetaFurby@users.noreply.github.com>
2022-10-08 13:49:13 -0500

    Added webpack-clean and html template

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,7 @@
+const path = require("path");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin");
 let mode = "development";
 let target = "web";
 if (process.env.NODE_ENV === "production") {
@@ -9,6 +12,7 @@ module.exports = {
     mode: mode,
     target: target,
     output: {
+        path: path.resolve(__dirname, "dist"),
         assetModuleFilename: "images/[hash][ext][query]"
     },
     module: {
@@ -31,14 +35,20 @@ module.exports = {
             },
             {
                 test: /\.jsx?$/,
-                exclude: /node_module/,
+                exclude: /node_modules/,
                 use: {
                     loader: "babel-loader",
                 }
             }
         ]
     },
-    plugins: [new MiniCssExtractPlugin()],
+    plugins: [
+        new CleanWebpackPlugin(),
+        new MiniCssExtractPlugin(),
+        new HtmlWebpackPlugin({
+            template: "./src/index.html",
+        }),
+    ],
     resolve: {
         extensions: [".js",".jsx"],
     },
