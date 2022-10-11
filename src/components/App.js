5ba5ab7d88042bc48ed60b87854dbb31e5acdf2e
Diego Collado <95520927+MetaFurby@users.noreply.github.com>
2022-10-10 20:30:40 -0500

    editorconfig and eslint react config

--- src/components/App.js
+++ src/components/App.js
@@ -2,18 +2,19 @@ import '../styles/index.scss';
 import Recipes from './Recipes';
 import houseImg from '../assets/house-1.png';
 import './App.scss';
+
 const App = () => {
-    return(
-        <>
-            <section className="house" />
-            <main>
-                <section>
-                    <h1 className="text-9xl">hi react</h1>
-                </section>
-                <img src={houseImg} alt="houseImg" width="250" />
-            </main>
-            <Recipes />
-        </>
-    );
+	return (
+		<>
+			<section className="house" />
+			<main>
+				<section>
+					<h1 className="text-9xl">hi react</h1>
+				</section>
+				<img src={houseImg} alt="houseImg" width="250" />
+			</main>
+			<Recipes />
+		</>
+	);
 };
 export default App;
