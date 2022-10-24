dca5ee3bf548c143aa4c24d21b156c8d6622748a
Diego Collado <>
2022-10-24 10:00:21 -0500

    Husky config

--- postcss.config.js
+++ postcss.config.js
@@ -1,9 +1,10 @@
 const tailwindcss = require('tailwindcss');
 const autoprefixer = require('autoprefixer');
+
 module.exports = {
-    plugins: [
-        "postcss-preset-env",
-        tailwindcss('./tailwind.config.js'),
-        autoprefixer,
-    ],
-}
+	plugins : [
+		'postcss-preset-env',
+		tailwindcss('./tailwind.config.js'),
+		autoprefixer,
+	],
+};
--- webpack.config.js
+++ webpack.config.js
@@ -1,64 +1,65 @@
-const path = require("path");
-const MiniCssExtractPlugin = require("mini-css-extract-plugin");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
-const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const ESLintPlugin = require('eslint-webpack-plugin');
-let mode = "development";
-let target = "web";
-if (process.env.NODE_ENV === "production") {
-    mode = "production";
-    target = "browserslist";
+
+let mode = 'development';
+let target = 'web';
+if (process.env.NODE_ENV === 'production') {
+	mode = 'production';
+	target = 'browserslist';
 }
 module.exports = {
-    mode: mode,
-    target: target,
-    output: {
-        path: path.resolve(__dirname, "dist"),
-        assetModuleFilename: "images/[hash][ext][query]"
-    },
-    module: {
-        rules: [
-            {
-                test: /\.(png|jpe?g|gif|svg)$/i,
-                type: "asset",
-            },
-            {
-                test: /\.(s[ac]|c)ss$/i,
-                use: [
-                    {
-                        loader: MiniCssExtractPlugin.loader,
-                        options: { publicPath : ""},
-                    },
-                    "css-loader",
-                    "sass-loader",
-                    "postcss-loader",
-                ],
-            },
-            {
-                test: /\.jsx?$/,
-                exclude: /node_modules/,
-                use: {
-                    loader: "babel-loader",
-                }
-            }
-        ]
-    },
-    plugins: [
-        new ESLintPlugin(),
-        new CleanWebpackPlugin(),
-        new MiniCssExtractPlugin(),
-        new HtmlWebpackPlugin({
-            template: "./src/index.html",
-        }),
-    ],
-    resolve: {
-        extensions: [".js",".jsx"],
-    },
-    devtool: "source-map",
-    devServer: {
-        static: "./dist",
-        hot: true,
-        port: 8080,
-        open: true,
-    }
-}
+	mode,
+	target,
+	output : {
+		path                : path.resolve(__dirname, 'dist'),
+		assetModuleFilename : 'images/[hash][ext][query]',
+	},
+	module : {
+		rules : [
+			{
+				test : /\.(png|jpe?g|gif|svg)$/i,
+				type : 'asset',
+			},
+			{
+				test : /\.(s[ac]|c)ss$/i,
+				use  : [
+					{
+						loader  : MiniCssExtractPlugin.loader,
+						options : { publicPath : '' },
+					},
+					'css-loader',
+					'sass-loader',
+					'postcss-loader',
+				],
+			},
+			{
+				test    : /\.jsx?$/,
+				exclude : /node_modules/,
+				use     : {
+					loader : 'babel-loader',
+				},
+			},
+		],
+	},
+	plugins : [
+		new ESLintPlugin(),
+		new CleanWebpackPlugin(),
+		new MiniCssExtractPlugin(),
+		new HtmlWebpackPlugin({
+			template : './src/index.html',
+		}),
+	],
+	resolve : {
+		extensions : ['.js', '.jsx'],
+	},
+	devtool   : 'source-map',
+	devServer : {
+		static : './dist',
+		hot    : true,
+		port   : 8080,
+		open   : true,
+	},
+};
